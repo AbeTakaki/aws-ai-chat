3f291dbbe4a3c258df46f5d0bd63fa72331787bf
AbeTakaki <abetakaki0101@gmail.com>
2026-03-23 16:18:42 +0900

    test用のコミット

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from 'vitest/config'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Vitestの設定を追加
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
