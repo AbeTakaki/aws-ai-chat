a12476425701f37f326a39804e6b8a51893ae1fc
AbeTakaki <abetakaki0101@gmail.com>
2026-04-27 14:36:07 +0900

    5sec

--- src/compoonents/ui/ChatInput.tsx
+++ src/compoonents/ui/ChatInput.tsx
@@ -1,11 +1,15 @@
 import { FaArrowUp } from "react-icons/fa";
-import { useState, type FormEvent } from "react";
+import { type FormEvent, useState } from "react";
 
 interface ChatInputProps {
   sendMessage: (message: string, model: string) => void;
+  initialModel?: string;
 }
 
-export default function ChatInput({ sendMessage }: ChatInputProps) {
+export default function ChatInput({
+  sendMessage,
+  initialModel,
+}: ChatInputProps) {
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
     if (!message.trim() || !selectedModel) {
@@ -17,44 +21,42 @@ export default function ChatInput({ sendMessage }: ChatInputProps) {
   const models = [
     {
       id: "global.anthropic.claude-haiku-4-5-20251001-v1:0",
-      name: "Claude 4.5 Haiku v1",
+      name: "Claude Haiku 4.5 v1",
     },
     {
       id: "global.anthropic.claude-sonnet-4-5-20250929-v1:0",
-      name: "Claude 4.5 Sonnet v1",
+      name: "Claude Sonnet 4.5 v1",
     },
     {
-      id: "apac.amazon.nova-pro-v1.0",
+      id: "apac.amazon.nova-pro-v1:0",
       name: "Amazon Nova Pro 1.0",
     },
   ];
-
   const [message, setMessage] = useState("");
-  const [selectedModel, setSelectedModel] = useState(models[0].id);
+  const [selectedModel, setSelectedModel] = useState(
+    initialModel || models[0].id,
+  );
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="flex flex-col justify-between gap-4 rounded-2xl border border-gray-200 p-4">
-        {
-          <textarea
-            name="message"
-            className="field-sizing-content max-h-80 w-full resize-none overflow-y-auto border-none break-words outline-none"
-            placeholder="質問を入力してください"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={(e) => {
-              if (e.nativeEvent.isComposing) return;
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                const form = e.currentTarget.closest("form");
-                if (form) {
-                  form.requestSubmit();
-                }
+        <textarea
+          name="message"
+          className="field-sizing-content max-h-80 w-full resize-none overflow-y-auto border-none break-words outline-none"
+          placeholder="質問を入力してください"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.nativeEvent.isComposing) return;
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              const form = e.currentTarget.closest("form");
+              if (form) {
+                form.requestSubmit();
               }
-            }}
-          />
-        }
-
+            }
+          }}
+        />
         <div className="flex justify-end gap-2">
           <div className="flex items-center justify-center rounded-md text-sm">
             <select
